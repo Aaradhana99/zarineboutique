5a8d695f47372569676d726e39e94df010ed1ba1
Aaradhana99 <aaradhanaallure99@gmail.com>
2025-05-01 07:36:37 +0530

    Add files via upload

---
+++ TestNGproject/src/NewTestcase/sample.java
@@ -0,0 +1,20 @@
+package NewTestcase;
+
+import org.testng.annotations.Test;
+
+public class sample {
+	
+	@Test
+	public void testcase1()
+	{
+		System.out.println("In test case 1");
+		
+	}
+
+	@Test
+	public void testcase2()
+	{
+		System.out.println("In test case 2");
+		
+	}
+}
